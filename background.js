80c47371ee1a86f8bbc43cf6ba9984219d1bc801
VarunBhandia <varunbhandia@gmail.com>
2019-01-18 14:46:18 +0530

    Initial Commit

--- background.js
+++ background.js
@@ -1,27 +1,3 @@
-console.log("running in bg");
+console.log("Email Creator background script running");
+const URL1 = "https://parser.zapier.com/mailboxes/"
 
-const URL1 = "https://www.amazon.com/RamFourBhandia-Sand-Trivia/dp/B07KP6GFVM/ref=sr_1_1?ie=UTF8&qid=1544688191&sr=8-1&keywords=sand+trivia+bhandia"
-//const URL2 = "https://www.amazon.com/BhandiaVarun-duster-trivia/dp/B07JH22KZH/ref=sr_1_1?ie=UTF8&qid=1540568378&sr=8-1&keywords=duster+trivia"
-const ALEXAWEBURL = "https://alexaweb.herokuapp.com/start"
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab) {	
-	console.log(tab);
-	chrome.tabs.sendMessage(tab.id, "hello")
-}
-
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(request, sender){
-//	if (request.stage === 1){
-//			chrome.tabs.update(sender.tab.id, { url: URL1 });
-//	}
-
-	if (request.stage === 2){
-			chrome.tabs.update(sender.tab.id, { url: URL1 });
-	}
-
-	if (request.stage === 3){
-		chrome.tabs.update(sender.tab.id, {url: ALEXAWEBURL})
-	}
-}
