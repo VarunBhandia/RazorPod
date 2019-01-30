d4adf685079ed572e0b63700db7157db97ef1fe6
Varun Bhandia <varunbhandia@gmail.com>
2019-01-30 18:17:23 +0530

    Implemented backgroud js to store data

--- background.js
+++ background.js
@@ -2,9 +2,7 @@ console.log("Internship Task background script running");
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(request, sender) {
+function gotMessage(request, sender){
+    console.log(request.data);
 
-    if (request.data) {
-        console.log("request.data");
-    }
 }
--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 console.log("Internship Task content script running");
 
+
 // write your keywork here
 let KEYWORD = 'mobiles';
 
@@ -7,35 +8,42 @@ let KEYWORD = 'mobiles';
 let BASEURL = "https://www.amazon.in/s/?field-keywords=" + KEYWORD + "&page=";
 
 if (location.href.substring(0, 53) == BASEURL) {
-  var E_PRICE = document.getElementsByClassName("a-size-base a-color-price s-price a-text-bold");
-  var E_NAME = document.getElementsByClassName("a-size-medium s-inline  s-access-title  a-text-normal");
-  // traverse the page and will collect information of products
-  for (var i = 0, len = E_PRICE.length - 1; i < len; i++) {
+    var E_PRICE = document.getElementsByClassName("a-size-base a-color-price s-price a-text-bold");
+    var E_NAME = document.getElementsByClassName("a-size-medium s-inline  s-access-title  a-text-normal");
+    // traverse the page and will collect information of products
+    for (var i = 0, len = E_PRICE.length - 1; i < len; i++) 
+    {
 
-    if (E_NAME[i]) {
-      let data1 = "NAME = " + E_NAME[i].innerText;
-      let data2 = "   PRICE = " + E_PRICE[i].innerText;
+        if (E_NAME[i]) 
+        {
+            let name = E_NAME[i].innerText;
+            let price = E_PRICE[i].innerText;
 
-      let data = data1 + data2;
+            let data = {
+                name: name,
+                price: price
+                
+            };
+            chrome.runtime.sendMessage({ data: data })
 
-      // console.log(data);
+            // console.log(data);
+        }
 
-      chrome.runtime.sendMessage({data: data});
     }
-  }
 
-  // this will change the URL and will traverse the <MAX_LIMIT> pages 
+    // this will change the URL and will traverse the <MAX_LIMIT> pages 
 
-  // to get the current  page number
-  var urlParams = new URLSearchParams(window.location.href);
-  let PAGE = parseInt(urlParams.get('page'));
+    // to get the current  page number
+    var urlParams = new URLSearchParams(window.location.href);
+    let PAGE = parseInt(urlParams.get('page'));
 
-  // define your maximum limit
-  let MAX_LIMIT = 20;
-  if (PAGE < MAX_LIMIT) {
-    PAGE = PAGE + 1;
-    BASEURL = "https://www.amazon.in/s/?field-keywords=" + KEYWORD + "&page=" + PAGE;
-    window.location.href = BASEURL;
-  }
+    // define your maximum limit
+    let MAX_LIMIT = 20;
+    if (PAGE < MAX_LIMIT) 
+    {
+        PAGE = PAGE + 1;
+        BASEURL = "https://www.amazon.in/s/?field-keywords=" + KEYWORD + "&page=" + PAGE;
+        window.location.href = BASEURL;
+    }
 
 }
