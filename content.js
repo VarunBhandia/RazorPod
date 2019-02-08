8added0ef5144f6e4fedfe8f9b6381593826e7de
Varun Bhandia <varunbhandia@gmail.com>
2019-02-08 09:46:01 +0530

    Added E_LINK in scrspped object

--- content.js
+++ content.js
@@ -10,6 +10,8 @@ let BASEURL = "https://www.amazon.in/s/?field-keywords=" + KEYWORD + "&page=";
 if (location.href.substring(0, 53) == BASEURL) {
     var E_PRICE = document.getElementsByClassName("a-size-base a-color-price s-price a-text-bold");
     var E_NAME = document.getElementsByClassName("a-size-medium s-inline  s-access-title  a-text-normal");
+    var E_LINK = document.getElementsByClassName("a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal");
+    
     // traverse the page and will collect information of products
     for (var i = 0, len = E_PRICE.length - 1; i < len; i++) 
     {
@@ -18,10 +20,12 @@ if (location.href.substring(0, 53) == BASEURL) {
         {
             let name = E_NAME[i].innerText;
             let price = E_PRICE[i].innerText;
+            let link = E_LINK[i].href;
 
             let data = {
                 name: name,
-                price: price
+                price: price,
+                link: link
 
             };
             chrome.runtime.sendMessage({ data: data })
